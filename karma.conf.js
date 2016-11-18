29c125bc468ca504393738edecec52a98ad0f6ce
Pedro <pedroob221@gmail.com>
2016-11-18 18:15:12 +0000

    Fixed test xml file

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
             foldAll: false // reports start folded (only with preserveDescribeNesting)
         },
         junitReporter: {
-            outputDir: 'test/xml-reports/TESTS-*.xml'
+            outputDir: 'test/xml-reports/'
         },
         port: 9876,
         colors: true,
