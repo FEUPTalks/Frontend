aa53e7f510a5a3ef5a67f84a70a43e8803d3e6b6
anna.denysyuk <anna.denysyuk@gmail.com>
2016-11-25 10:22:59 +0000

    list_edit

--- app/main/talks/list_edit/list_edit.component.ts
+++ app/main/talks/list_edit/list_edit.component.ts
@@ -7,6 +7,9 @@ import { Headers, RequestOptions }        from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import "rxjs/add/operator/map";
 
+import {TalkService} from "../../../services/talk.service";
+import {Talk} from "../../../services/talk";
+
 @Component({
     moduleId: module.id,
     selector: 'talk-list_edit-cmp',
@@ -16,6 +19,8 @@ import "rxjs/add/operator/map";
 
 @Injectable()
 export class List_EditComponent {
+
+talks = {};
 
  constructor(private http:Http) {
 
@@ -27,14 +32,24 @@ export class List_EditComponent {
         var req = {};
         req['title'] = "test";
         let body = JSON.stringify(req);     
-        return this.http.get('http://les16b.fe.up.pt/talks').map(res => res.json())
+
+        this.http.get('http://les16b.fe.up.pt:8144/talks').toPromise().then(res => {
+            this.talks = res.json();
+        });
+
+        
+
+        /*
+        return this.http.get('http://les16b.fe.up.pt:8144/talks').map(res => res.json())
         .subscribe(
                 data => req['response'] = "not sure how to get response yet",
                 err => console.log('ERROR!!!'),
                 () => console.log("Finished")
 
              );
-          }
+             */
+          
+    }
 
      editTalks(){
 
@@ -46,7 +61,7 @@ export class List_EditComponent {
          var req = {};
          req['title'] = "test";
          let body = JSON.stringify(req);
-         return this.http.put('http://les16b.fe.up.pt/talks' + body, headers).map((res:Response) => res.json());
+         return this.http.put('http://les16b.fe.up.pt:8144/talks' + body, headers).map((res:Response) => res.json());
        
      }
 
