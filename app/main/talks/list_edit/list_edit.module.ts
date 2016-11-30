c6ab9ea2316f4429fd6fb8768be3909ed97e302f
Pedro <pedroob221@gmail.com>
2016-11-30 17:34:48 +0000

    Search from available talks

--- app/main/talks/list_edit/list_edit.module.ts
+++ app/main/talks/list_edit/list_edit.module.ts
@@ -2,13 +2,15 @@ import { NgModule }     from '@angular/core';
 import { HttpModule }   from '@angular/http';
 
 import { List_EditComponent } from './list_edit.component';
-import {CommonModule} from "@angular/common";
-import { TalkService }           from "../../services/talk.service";
+import { CommonModule } from "@angular/common";
+import { TalkService }           from "../../../services/talk.service";
+
 
 @NgModule({
     imports: [
         HttpModule,
-        CommonModule
+        CommonModule,
+        Angular2DataTableModule
     ],
     declarations: [List_EditComponent],
     providers: [TalkService],
