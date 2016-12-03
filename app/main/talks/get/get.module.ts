89d3b85b6ce9b97010d8d6b5109df451168867be
Pedro <pedroob221@gmail.com>
2016-12-03 20:55:11 +0000

    New page for each talk

---
+++ app/main/talks/get/get.module.ts
@@ -0,0 +1,20 @@
+import { NgModule }     from '@angular/core';
+import { HttpModule }   from '@angular/http';
+
+import { List_EditComponent } from './list_edit.component';
+import { TalkService }           from "../../../services/talk.service";
+import {SharedModule} from "../../../shared/shared/shared.module";
+import {Angular2DataTableModule} from "angular2-data-table";
+
+@NgModule({
+    imports: [
+        SharedModule,
+        HttpModule,
+        Angular2DataTableModule
+    ],
+    declarations: [List_EditComponent],
+    providers: [TalkService],
+    exports: [List_EditComponent]
+})
+
+export class List_EditModule { }
