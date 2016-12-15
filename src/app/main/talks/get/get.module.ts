50ad5096c14e90e1407694fa76814f872f779eb4
Pedro <pedroob221@gmail.com>
2016-12-15 18:25:38 +0000

    Exportas Fix

--- src/app/main/talks/get/get.module.ts
+++ src/app/main/talks/get/get.module.ts
@@ -5,12 +5,14 @@ import { TalkService }           from "../../../services/api/talk.service";
 import {SharedModule} from "../../../shared/shared/shared.module";
 import {TalkGetComponent} from "./get.component";
 import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         SharedModule,
         RouterModule,
         HttpModule,
+        FormsModule
     ],
     declarations: [TalkGetComponent],
     providers: [TalkService],
